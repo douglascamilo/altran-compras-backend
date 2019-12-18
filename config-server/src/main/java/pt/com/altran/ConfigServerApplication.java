0207c1d0134eefa285fa9111b5b86f0186af8541
Douglas Sousa <douglascamilo@gmail.com>
2019-12-18 01:12:37 -0300

    Criação do Servidor de Configurações

---
+++ config-server/src/main/java/pt/com/altran/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package pt.com.altran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
